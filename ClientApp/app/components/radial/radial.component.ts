414dcbcd1cb783bf9c24642fe7bd7e85b7ae923f
Ponomareva <natalia.ponomareva@arcadia.spb.ru>
2018-06-18 13:52:56 +0300

    Updated radial config

--- ClientApp/app/components/radial/radial.component.ts
+++ ClientApp/app/components/radial/radial.component.ts
@@ -1,23 +1,9 @@
 import { Component, Inject,
     Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from "@angular/core";
 import * as d3 from "d3";
-import { HierarchyNode, HierarchyRectangularNode, range } from "d3";
 import { DataService } from "../data.service";
 import { IHierarchy } from "../iris";
-
-const sizes = {
-    vHeight: 500,
-    vWidth: 500,
-};
-const config = {
-    arc: d3.arc<HierarchyRectangularNode<{}>>()
-            .startAngle((d) => d.x0)
-            .endAngle((d) => d.x1)
-            .innerRadius((d) => d.y0)
-            .outerRadius((d) => d.y1),
-    color: d3.scaleOrdinal(d3.schemeCategory10),
-    radius: (Math.min(sizes.vWidth, sizes.vHeight) / 2) - 10,
-};
+import * as config from "./radial.config";
 
 @Component({
     selector: "radial",
@@ -33,25 +19,7 @@ export class RadialComponent implements OnChanges, OnInit {
     }
 
     public ngOnInit() {
-        this.data.currentPlotMessage.subscribe((message) => this.drawBigDots(message));
-    }
-
-    public drawBigDots(message: string) {
-        const splitted = message.split(" ");
-        if (this.result) {
-            switch (splitted[0]) {
-                case "start":
-                    this.result.style("fill", "none");
-                    break;
-                case "on":
-                    this.result.filter((d) => splitted[1].lastIndexOf((d.data as IHierarchy).id, 0) === 0)
-                        .style("fill", (d) => config.color((d.data as IHierarchy).species + d.value + d.depth));
-                    break;
-                case "stop":
-                    this.result.style("fill", (d) => config.color((d.data as IHierarchy).species + d.value + d.depth));
-                    break;
-            }
-        }
+        this.data.currentPlotMessage.subscribe((msg) => this.result ? config.getMessage(this.result, msg) : null);
     }
 
     public ngOnChanges(changes: SimpleChanges) {
@@ -61,42 +29,11 @@ export class RadialComponent implements OnChanges, OnInit {
     }
 
     public draw() {
-        const g = d3.select("svg")
-            .attr("width", sizes.vWidth)
-            .attr("height", sizes.vHeight)
-            .append("g")
-            .attr("transform", "translate(" + sizes.vWidth / 2 + "," + sizes.vHeight / 2 + ")");
-        const tooltip = d3.select("div").append("div")
-            .attr("class", "tooltip")
-            .style("opacity", 0);
-
-        // Find data root
         const root = d3.hierarchy<IHierarchy>(this.irises as IHierarchy,
             (d: IHierarchy) => d.children ? d.children : null)
             .sum((d: IHierarchy) => 1);
         const desc = d3.partition().size([2 * Math.PI, config.radius])(root).descendants();
-
-        this.result = g.selectAll("circle")
-            .data(desc).enter().append("path")
-            .attr("display", (d) => d.depth ? null : "none")
-            .attr("id", (d, i) => "radial" + (d.data as IHierarchy).id)
-            .attr("d", config.arc)
-            .style("stroke", "#fff")
-            .style("fill", (d) => config.color((d.data as IHierarchy).species + d.value + d.depth))
-            .on("mouseover", (d, i) => {
-                this.data.sendRadial("on " + (d.data as IHierarchy).id);
-                tooltip.transition()
-                    .duration(400)
-                    .style("opacity", .9);
-                tooltip.html((d.data as IHierarchy).species)
-                    .style("left", d3.event.pageX + "px")
-                    .style("top", d3.event.pageY + "px");
-            })
-            .on("mouseout", (d, i) => {
-                this.data.sendRadial("out " + (d.data as IHierarchy).id);
-                tooltip.transition()
-                    .duration(400)
-                    .style("opacity", 0);
-            });
+        const svg = config.getSvg();
+        this.result = config.getData(svg, desc, (msg) => this.result ? config.getMessage(this.result, msg) : null);
     }
 }
---
+++ ClientApp/app/components/radial/radial.config.ts
@@ -0,0 +1,73 @@
+import * as d3 from "d3";
+import { IHierarchy } from "../iris";
+
+const sizes = {
+    vHeight: 500,
+    vWidth: 500,
+};
+const config = {
+    arc: d3.arc<d3.HierarchyRectangularNode<{}>>()
+            .startAngle((d) => d.x0)
+            .endAngle((d) => d.x1)
+            .innerRadius((d) => d.y0)
+            .outerRadius((d) => d.y1),
+    color: d3.scaleOrdinal(d3.schemeCategory10),
+};
+const tooltip = d3.select("div").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+export const radius = (Math.min(sizes.vWidth, sizes.vHeight) / 2) - 10;
+
+export const getSvg = () => {
+    return d3.select("svg")
+            .attr("width", sizes.vWidth)
+            .attr("height", sizes.vHeight)
+            .append("g")
+            .attr("transform", "translate(" + sizes.vWidth / 2 + "," + sizes.vHeight / 2 + ")");
+};
+
+export const getData = (svg: d3.Selection<d3.BaseType, {}, HTMLElement, any>,
+                        data: Array<d3.HierarchyRectangularNode<{}>>,
+                        send: (msg: string) => void) => {
+    return svg.selectAll("circle")
+        .data(data).enter().append("path")
+        .attr("display", (d: d3.HierarchyRectangularNode<{}>) => d.depth ? null : "none")
+        .attr("id", (d: d3.HierarchyRectangularNode<{}>) => "radial" + (d.data as IHierarchy).id)
+        .attr("d", config.arc)
+        .style("stroke", "#fff")
+        .style("fill", (d: d3.HierarchyRectangularNode<{}>) =>
+            config.color((d.data as IHierarchy).species + d.value + d.depth))
+        .on("mouseover", (d: d3.HierarchyRectangularNode<{}>) => {
+            send("on " + (d.data as IHierarchy).id);
+            tooltip.transition()
+                .duration(400)
+                .style("opacity", .9);
+            tooltip.html((d.data as IHierarchy).species)
+                .style("left", d3.event.pageX + "px")
+                .style("top", d3.event.pageY + "px");
+        })
+        .on("mouseout", (d: d3.HierarchyRectangularNode<{}>) => {
+            send("out " + (d.data as IHierarchy).id);
+            tooltip.transition()
+                .duration(400)
+                .style("opacity", 0);
+        });
+};
+
+export const getMessage = (result: d3.Selection<d3.BaseType, d3.HierarchyRectangularNode<{}>, d3.BaseType, {}>,
+                           msg: string) => {
+    const splitted = msg.split(" ");
+    switch (splitted[0]) {
+        case "start":
+            result.style("fill", "none");
+            break;
+        case "on":
+            result.filter((d) => splitted[1].lastIndexOf((d.data as IHierarchy).id, 0) === 0)
+                .style("fill", (d) => config.color((d.data as IHierarchy).species + d.value + d.depth));
+            break;
+        case "stop":
+            result.style("fill", (d) => config.color((d.data as IHierarchy).species + d.value + d.depth));
+            break;
+    }
+};
